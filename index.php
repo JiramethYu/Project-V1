a4eb88908cba973bbec34b6fe5c841aa6cdcbf93
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-09-06 00:28:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 <body>
 <h1>62101456 Jirameth Yukachain</h1>
  <?php
-$api_url = 'https://api.thingspeak.com/channels/1491788/feeds.json?api_key=74VCZK50UOXG0HO4&results=1';
+$api_url = ' https://api.thingspeak.com/channels/1497866/feeds.json?results=1';
 $json_data = file_get_contents($api_url);
 $response_data = json_decode($json_data);
 $user_data = $response_data->feeds;
 $user_data = array_slice($user_data, 0);
 ?>  
-<iframe src="https://api.thingspeak.com/channels/1491788/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
+<iframe src="https://thingspeak.com/channels/1497866/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
 height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
  <?php
    foreach ($user_data as $user) {
@@ -22,7 +22,7 @@ height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
       echo " C ";
    }?> 
  <br>   
-<iframe src="https://api.thingspeak.com/channels/1491788/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="250"
-width="500" frameBorder="0" title="Iframe Example"></iframe>
+<iframe src="https://thingspeak.com/channels/1497866/maps/channel_show" height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
+<br>         
 </body>
 </html>   
